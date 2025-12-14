652eccb99a072356b7f52612b8a5ee103047f922
Nguyen Minh Tuan <minhtuan.nguyen130396@gmail.com>
2025-12-15 00:31:26 +0700

    finish subscribe and call  by ip , got and error in destructor

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstdint>
-#include "readWrite/ReadWriteAPI.h"
+#include "observer.cpp"
 
-//ReadWriteAPI* ReadWriteAPI::Instance = NULL; -> da dinh nghia o file nguon de dam bao tinh dong goi
-
-int main()
-{
-	ReadWriteAPI* readWriteAPI = ReadWriteAPI::getInstance();
-	
-	//a=readWriteAPI->readFile("E:\\Sever\\in.txt");
-	// std::cout <<std::endl<<a<<std::endl;
-	std::cout<<"Hello world";
-	return 0;
+int main (){
+    std::cout<<"Hello world!"<<std::endl;
+    return 0;
 }
---
+++ observer.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <memory>
+#include <map>
+#include <vector>
+
+class Observer{
+public:
+    virtual void hasChange() =0;
+    virtual ~Observer() = default;
+};
+
+class Subject{
+public:
+    virtual void subscribe(void* ptr, std::string & ip_address) =0;
+    virtual void unsubscribe(void* ptr) =0;
+    virtual void notifyAll()=0;
+    virtual void notifyByIp(const std::string& ip) =0;
+};
+#define MAX_IP_SIZE 255
+class Server: public Subject{
+private:
+    std::vector<std::shared_ptr<std::pair<std::string, Observer*>>> list_observer;
+    std::string get_ip_address(){
+        if (list_observer.size() > MAX_IP_SIZE){
+            std::cout<<"Runout of command\n";
+            return "";
+        }
+        if(list_observer.size() == 0){
+            std::cout <<"has ip 1.1.1.2 available\n";
+            return "1.1.1.2";
+        }
+        int ip_index_num = 3;
+        for (ip_index_num;ip_index_num < MAX_IP_SIZE; ip_index_num ++){
+            bool is_ip_used = false ;
+            std::string ip_address = "1.1.1." + std::to_string(ip_index_num);
+            for (auto it: list_observer){
+                if( it && it->first == ip_address) is_ip_used = true;
+            }
+            if(is_ip_used) continue;
+            std::cout << "new address declared: "<<ip_address <<"\n";
+            return ip_address;
+        }
+        return "";
+
+    }
+public:
+    void subscribe(void* ptr, std::string & ip_address) override{
+        if(!ptr) return;
+        Observer* observer = static_cast<Observer*> (ptr);
+        std::string _ip_address = get_ip_address();
+        if(_ip_address == "") {
+            return;
+        }
+        std::shared_ptr<std::pair<std::string,Observer*>> p_observer =std::make_shared<std::pair<std::string,Observer*>>( _ip_address,observer );
+        list_observer.push_back(p_observer);
+
+        std::cout <<"created new client ip: "<<p_observer->first<<"\n";
+        ip_address =  _ip_address;
+    }
+
+    void unsubscribe(void * ptr) override{
+        if(!ptr) return;
+        bool isOk = false;
+        Observer* observer = static_cast<Observer*> (ptr);
+        for (int i =0 ; i < list_observer.size(); i++){
+            if(ptr == list_observer.at(i)->second){
+                isOk = true;
+                list_observer.erase(list_observer.begin() + i);
+                std::cout<< "Removed Ip: "<< list_observer.at(i)->first;
+            }
+        }
+    }
+    void notifyAll() override {
+        for (auto it: list_observer){
+            it->second->hasChange();
+        }
+    }
+    void notifyByIp(const std::string& ip) override{
+        Observer* observer = nullptr;
+        for (auto it: list_observer){
+            if(it->first == ip){
+                observer = it->second;
+                observer->hasChange();
+            }
+        }
+    }
+};
+class Client:public Observer{
+    private:
+    std::string ip;
+    Subject* subject;
+    public:
+    Client(void * ptr ){
+        ip = "";
+        subject  = static_cast<Subject*> (ptr);
+        subject->subscribe(this, ip);
+        std::cout<<"[Client] subscribe success\n";
+    }
+    ~Client(){
+        subject->unsubscribe(this);
+
+    }
+    void hasChange() override {
+        printf("[Client] Ip: %s, Receive command from server\n", ip.c_str());
+    }
+};
+int main(){
+    Subject* subject = new Server();
+    Observer* observer1 = new Client(subject);
+    Observer* observer2 = new Client(subject);
+    Observer* observer3 = new Client(subject);
+    Observer* observer4 = new Client(subject);
+    subject->notifyAll();
+    subject->notifyByIp("1.1..3");
+    delete(observer4);
+    delete(observer3);
+    delete(observer2);
+    delete(observer1);
+    
+    
+}
